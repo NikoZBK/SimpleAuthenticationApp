0721dc0821e7e66233b3cfb91464cc1cf7fb22b7
NikoZBK <nikozbk@gmail.com>
2025-02-01 17:48:34 -0500

    Implement refresh_token endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -113,9 +113,62 @@ router.post('/logout', async (_req, res) => {
 // REFRESH ACCESS TOKEN
 router.post('/refresh_token', async (req, res) => {
   try {
-    //TODO
+    const { refreshtoken } = req.cookies;
+
+    if (!refreshtoken) {
+      return res.status(500).json({
+        message: 'No refresh token!',
+        status: 'error',
+      });
+    }
+
+    // Refresh token successfully created, so verify it
+    let id;
+    try {
+      id = verify(refreshtoken, process.env.REFRESH_TOKEN_SECRET).id;
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Invalid refresh token!',
+        status: 'error',
+      });
+    }
+    // Refresh token is valid, so check if the user even exists
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(500).json({
+        message: "User doesn't exist!",
+        status: 'error',
+      });
+    }
+
+    // User exists, so now check if the refresh token is correct
+    if (user.refreshtoken !== refreshtoken) {
+      return res.status(500).json({
+        message: 'Invalid refresh token!',
+        status: 'error',
+      });
+    }
+
+    // Refresh token is correct, so now create new tokens
+    const accessToken = createAccessToken(user._id);
+    const refreshToken = createRefreshToken(user._id);
+
+    // Update the user's refresh token
+    user.refreshtoken = refreshToken;
+
+    // Finally, send the new tokens in the response
+    sendRefreshToken(res, refreshToken);
+
+    return res.json({
+      message: 'Refreshed successfully!',
+      status: 'success',
+      accessToken,
+    });
   } catch (error) {
-    //TODO: General refresh token error response
+    res.status(500).json({
+      message: 'Error refreshing token!',
+      type: 'error',
+    });
   }
 });
 
