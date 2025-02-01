8ff3bfcb4316a3dc538073e1bd832c2bce692980
NikoZBK <nikozbk@gmail.com>
2025-01-31 20:04:06 -0500

    Add user model, authentication routes, and MongoDB connection setup

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,49 @@
-var express = require("express");
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { hash, compare } = require('bcryptjs');
+const {
+  createAccessToken,
+  createRefreshToken,
+  createEmailVerifyToken,
+  sendAccessToken,
+  sendRefreshToken,
+  sendEmailToken,
+} = require('../utils/tokens');
+// import the user model
+const User = require('../models/user');
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Check if user exists first
+    const user = await User.findOne({ email: email });
+    // User already exists, get outta here
+    if (user) {
+      return res.status(500).json({
+        message: 'Account already exists, please try logging in.',
+        type: 'warning',
+      });
+    }
+    // User does not exist, so create the user and hash the password
+    const passwordHash = await hash(password, 10);
+    const newUser = new User({
+      email: email,
+      password: passwordHash,
+    });
+    // Save user to db
+    await newUser.save();
+    // Successfully saved user
+    res.status(200).json({
+      message: 'User created successfully!',
+      type: 'success',
+    });
+  } catch (error) {
+    res.status(500).json({
+      type: 'error',
+      message: 'Error creating user!',
+      error,
+    });
+  }
+});
 
 module.exports = router;
---
+++ utils/tokens.js
@@ -0,0 +1,49 @@
+const { sign } = require('jsonwebtoken');
+
+const createAccessToken = id => {
+  return sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: 15 * 60,
+  });
+};
+
+const createRefreshToken = id => {
+  return sign({ id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: '90d',
+  });
+};
+
+const createEmailVerifyToken = id => {
+  return sign({ id }, process.env.EMAIL_VERIFY_TOKEN_SECRET, {
+    expiresIn: 15 * 60,
+  });
+};
+
+const sendAccessToken = (_req, res, accessToken) => {
+  res.json({
+    accessToken,
+    message: 'Sign in Successful',
+    type: 'success',
+  });
+};
+
+const sendRefreshToken = (res, refreshToken) => {
+  res.cookie('refreshtoken', refreshToken, {
+    httpOnly: true,
+  });
+};
+
+const sendEmailVerifyToken = (_req, res, emailVerifyToken) => {
+  res.json({
+    emailVerifyToken,
+    message: 'Email Verified',
+    type: 'success',
+  });
+};
+
+module.exports = {
+  createAccessToken,
+  createRefreshToken,
+  createEmailVerifyToken,
+  sendAccessToken,
+  sendRefreshToken,
+};
