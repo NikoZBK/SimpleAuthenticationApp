4e919fcbb43d9c190068863f19b33a5a015ebeb9
NikoZBK <nikozbk@gmail.com>
2025-02-01 19:26:15 -0500

    Implement email utility functions for password reset email support.

---
+++ utils/email.js
@@ -0,0 +1,57 @@
+const { createTransport } = require('nodemailer');
+
+const createPasswordResetUrl = (id, token) =>
+  `${process.env.CLIENT_URL}/reset-password/${id}/${token}`;
+
+const transporter = createTransport({
+  service: process.env.EMAIL_HOST,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const passwordResetTemplate = (user, url) => {
+  const { username, email } = user;
+  return {
+    from: `Mail - <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: `Reset Password`,
+    html: `
+        <h2>Password Reset Link</h2>
+        <p>Reset your password by clicking on the link below:</p>
+        <a href=${url}><button>Reset Password</button></a>
+        <br />
+        <br />
+        <small><a style="color: #38A169" href=${url}>${url}</a></small>
+        <br />
+        <small>The link will expire in 15 mins!</small>
+        <small>If you haven't requested a password reset, please make sure your account is secure, otherwise you can ignore this email.</small>
+        <br /><br />
+        <p>Thanks,</p>
+        <p>Simple Authentication App by NikoZBK</p>`,
+  };
+};
+
+const passwordResetConfirmationTemplate = user => {
+  const { email } = user;
+  return {
+    from: `Mail - <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: `Password Reset Successful`,
+    html: `
+        <h2>Password Reset Successful</h2>
+        <p>You've successfully updated your password for account: <${email}>. </p>
+        <small>If this wasn't you, someone may have gained unauthorized access to your account. Please secure your email account as soon as possible before resetting your password.</small>
+        <br /><br />
+        <p>Thanks,</p>
+        <p>Simple Authentication App by NikoZBK</p>`,
+  };
+};
+
+module.exports = {
+  transporter,
+  createPasswordResetUrl,
+  passwordResetTemplate,
+  passwordResetConfirmationTemplate,
+};
